86657e623ecedfaaf9c2f011a88ef314ce21fbe2
Victor <victor.gawk@gmail.com>
2026-02-14 14:55:29 -0300

    fix jaxe build

--- packages/sx05re/libretro/jaxe/package.mk
+++ packages/sx05re/libretro/jaxe/package.mk
@@ -2,7 +2,7 @@
 # EmuELEC / LibreELEC package for JAXE libretro core
 
 PKG_NAME="jaxe"
-PKG_VERSION="4825aad24716f67924cd949354aae490a14b4d2d"
+PKG_VERSION="e8e90e3d683bb560df5882f0ad62ed28f96a541a"
 PKG_ARCH="any"
 PKG_LICENSE="MIT"
 PKG_SITE="https://github.com/kurtjd/jaxe"
